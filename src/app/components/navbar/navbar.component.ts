c4be5120f857ecca780c1fb7f433e40d9b1d9bdc
Lasse Valtanen <lasse.valtanen@gmail.com>
2020-03-04 13:30:33 +0200

    navbar changes, toggle works

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,4 +11,19 @@ export class NavbarComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit() {}
+
+	navState() {
+		var element = document.getElementById("buttonCollapse");
+		var element2 = document.getElementById("navigationCollapse");
+
+		if (element.getAttribute('aria-expanded') === 'true') {
+			element.setAttribute('aria-expanded', 'false');
+			element.setAttribute('class', 'navbar-toggler');
+		}
+
+		if (element2.className === 'navbar-collapse justify-content-end collapse show') {
+		element2.setAttribute('class', 'navbar-collapse justify-content-end collapsing');
+		}
+
+	}
 }
