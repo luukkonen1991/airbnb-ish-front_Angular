416e0f3373823821e064d2345e58cf365c0c895c
Lasse Valtanen <lasse.valtanen@gmail.com>
2020-03-18 10:08:39 +0200

    created a proper become a host page

--- src/app/components/become-host/become-host.component.ts
+++ src/app/components/become-host/become-host.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
+import { DataService } from 'src/app/services/data.service';
+
+import { AuthRegister } from '../../models/AuthRegister';
 
 @Component({
   selector: 'app-become-host',
@@ -6,10 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./become-host.component.css']
 })
 export class BecomeHostComponent implements OnInit {
+	authRegister: AuthRegister = {
+		name: '',
+		email: '',
+    password: '',
+    role: 'publisher'
+	};
+	form: any;
+	errorState: string = '';
 
-  constructor() { }
+	constructor(private authService: AuthService, private router: Router, private dataService: DataService) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	onSubmit() {
+		this.authService.registerUser(this.authRegister).subscribe(
+			res => {
+				if (res.success === true) {
+					sessionStorage.setItem('token', res.token);
+					this.dataService.showNotification('Successfully logged in!', true);
+					this.router.navigate([
+						'/me'
+					]);
+				}
+			},
+			error => {
+				if (error.error.success === false) {
+					this.dataService.showNotification(error.error.error, false);
+					this.errorState = error.error.error;
+				}
+			}
+		);
+	}
 }
